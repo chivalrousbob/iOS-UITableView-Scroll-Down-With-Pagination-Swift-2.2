33d2e4a9617ea09b5e61a4c42966ad87239004df
mac <mac@macbook-pro-de-mac.local>
2016-08-04 21:30:16 +0100

    Initial Commit

---
+++ TrivagoProjectByNouriAyoubTests/TrivagoProjectByNouriAyoubTests.swift
@@ -0,0 +1,36 @@
+//
+//  TrivagoProjectByNouriAyoubTests.swift
+//  TrivagoProjectByNouriAyoubTests
+//
+//  Created by mac on 04/08/16.
+//  Copyright © 2016 AYOUB NOURI. All rights reserved.
+//
+
+import XCTest
+@testable import TrivagoProjectByNouriAyoub
+
+class TrivagoProjectByNouriAyoubTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
